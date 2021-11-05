4b924955c0cb9fb75b5c1ef39a1d62300644f44a
wfy <wangfangyuan@matrixelements.com>
2021-11-05 19:00:34 +0800

    feat: add data detail authValueStr

--- src/views/home/components/DataDetail.vue
+++ src/views/home/components/DataDetail.vue
@@ -4,8 +4,8 @@
       <div class="title">{{ item.title }}</div>
       <div class="item-info" v-if="item.describes && item.describes.length > 0">
         <div v-for="(desc, i) in item.describes" :key="i">
-          <div class="lable">{{ desc.lable }}</div>
-          <div class="info">{{ data[desc.value] }}</div>
+          <div class="lable" v-if="data[desc.value]">{{ desc.lable }}</div>
+          <div class="info" v-if="data[desc.value]">{{ data[desc.value] }}</div>
         </div>
       </div>
       <div class="item-info" v-else>{{ data[item.describe] }}</div>
@@ -94,6 +94,10 @@ export default class DataDetail extends Vue {
         {
           lable: '支持授权方式：',
           value: 'authType',
+        },
+        {
+          lable: '授权值：',
+          value: 'authValueStr',
         },
       ],
     },
