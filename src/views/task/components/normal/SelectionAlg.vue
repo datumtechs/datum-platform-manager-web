6c8186684b8979491b9b9cf930d973a85ed5db18
maxingcong <41977768+maxingcong@users.noreply.github.com>
2022-04-21 11:17:01 +0800

    add Wizard Mode interface

---
+++ src/views/task/components/normal/SelectionAlg.vue
@@ -0,0 +1,165 @@
+<template>
+  <el-form v-bind="$attrs" :label-position="'top'" :rules="rules" :ref="(el: any) => formRef = el" :model="form"
+    class="mt-38px">
+    <el-form-item :label="`${t('task.taskName')}:`" prop="workflowName">
+      <div class="absolute -top-30px cursor-pointer" :style="{ left: locale === 'zh' ? '142px' : '161px' }">
+        <question-mark>
+          <template #content>
+            {{ t('task.taskNameRules') }}
+            <br />
+            1、{{ t('task.taskNameRulesOne') }}
+            <br />
+            2、{{ t('task.taskNameRulesTwo') }}
+            <br />
+            3、{{ t('task.taskNameRulesThree') }}
+            <br />
+            4、{{ t('task.taskNameRulesFour') }}
+            <br />
+          </template>
+        </question-mark>
+      </div>
+      <el-input :input-style="{ borderColor: '#EEEEEE', height: '50px' }" :minlength="8" :maxlength="64"
+        v-model="form.workflowName"></el-input>
+    </el-form-item>
+    <el-form-item :label="`${t('task.stepOneSelectComputingTitle')}:`" prop="calculationType">
+      <el-radio-group v-model="form.calculationType"
+        @change="form.algorithmId = undefined, form.calculationProcessId = undefined">
+        <el-radio :label="item.id" v-for="item in algList" :key="item.id">{{ item.name }}</el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item v-if="form.calculationType == 1000" :label="`${$t('task.stepOneSelectAlgorithmTitle')}:`"
+      prop="algorithmId">
+      <el-radio-group v-model="form.algorithmId" @change="algChange">
+        <el-radio :label="item.id" v-for="item in algTypelist[0].childrenList" :key="item.id">{{ item.name }}</el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item v-if="form.calculationType == 2000" :label="`${t('task.stepOneSelectAIAlgorithmTitle')}:`"
+      prop="algorithmId">
+      <el-radio-group v-model="form.algorithmId" @change="algChange">
+        <el-radio :label="item.id" v-for="item in algTypelist[0].childrenList" :key="item.id">{{ item.name }}
+        </el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item v-if="processList.length" :label="`${$t('task.stepOneSelectProcedureTitle')}:`"
+      prop="calculationProcessId">
+      <el-radio-group v-model="form.calculationProcessId">
+        <el-radio :label="item.calculationProcessId" v-for="item in processList" :key="item.id">{{ item.name }}
+        </el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item :label="`${t('task.pleaseComments')}:`">
+      <el-input :input-style="{ borderColor: '#EEEEEE', height: '50px' }" show-word-limit type="textarea" :rows="4"
+        v-model="form.workflowDesc"></el-input>
+    </el-form-item>
+  </el-form>
+  <NextButton @click="next" />
+</template>
+<script lang="ts" setup>
+import { getAlgTree, } from '@/api/algorithm'
+import { postCreateWorkflowWizard, getProcessList } from '@/api/workflow'
+import NextButton from './NextButton.vue'
+const { locale, t } = useI18n()
+const emit = defineEmits(["getParams", 'next', 'getStep'])
+
+const algList: any = ref<any[]>([])//算法列表 //最外层
+//具体算法
+const algTypelist = computed(() => algList.value.filter((v: any) => v.id == form.calculationType))
+const processList = ref<any[]>([])//计算流程列表
+
+watch(locale, () => {
+  formRef.value.clearValidate()
+})
+
+const form: any = reactive({
+  workflowName: "",
+  calculationType: undefined,
+  algorithmId: undefined,
+  calculationProcessId: undefined,
+  workflowDesc: ""
+})
+
+const formRef = ref()
+const rules = ref({
+  workflowName: [
+    {
+      validator: (rule: any, value: any, callback: any) => {
+        if (value && (String(value).length) < 8) {
+          return callback(t('task.taskNameRulesThree'))
+        }
+        return callback()
+      },
+    },
+    { required: true, message: `${t('task.pleaseEnter')}${t('task.taskName')}` },
+  ],
+  calculationType: [{ required: true, message: t('task.stepOneSelectComputingTitle') }],
+  algorithmId: [{ required: true, message: t('task.stepOneSelectAlgorithmTitle') }],
+  calculationProcessId: [{ required: true, message: t('task.stepOneSelectProcedureTitle') }]
+})
+
+const next = () => {
+  emit('getStep', {
+    list: [
+      { step: 1, type: 2 },
+      { step: 2, type: 3 },
+      { step: 3, type: 5 }
+    ],
+    algPSI: true
+  })
+  return
+
+  // console.log(formRef.value.validate)
+  // emit('getParams', Object.assign({}, form.value))
+  // console.log(Object.assign({}, form.value))
+  formRef.value.validate().then((v: any) => {
+    // console.log(v, form)
+    submit()
+    // emit('getParams', Object.assign({}, form))
+    // emit('next')
+  })
+}
+
+const submit = () => {
+  postCreateWorkflowWizard({
+    "algorithmId": form.algorithmId,
+    "calculationProcessId": form.calculationProcessId || 0,
+    "workflowDesc": form.workflowDesc,
+    "workflowName": form.workflowName,
+  }).then(res => {
+    const { data, code } = res
+    if (code == 10000) {
+      processList.value.forEach(v => {
+        if (v.calculationProcessId == form.calculationProcessId) {
+          emit('getStep', { list: v.stepItem, algPSI: v.name.indexOf('PSI') > -1 })
+        }
+      })
+    }
+  })
+}
+
+const queryAlgTree = () => {
+  getAlgTree().then(res => {
+    const { data, code } = res
+    if (code === 10000) {
+      algList.value = data?.childrenList[0]?.childrenList
+    }
+  })
+}
+
+const algChange = () => {
+  getProcessList({ algorithmId: form.algorithmId }).then(res => {
+    const { data, code } = res
+    if (code === 10000) {
+      form.calculationProcessId = undefined
+      processList.value = data
+      // processShow.value = data.length ? true : false
+    }
+  })
+}
+
+onMounted(() => {
+  queryAlgTree()
+})
+
+</script>
+<style lang="scss" scoped>
+</style>
