a18dc7c11d1dcb54a5267edd3221ef75eff8e4e9
maxingcong <41977768+maxingcong@users.noreply.github.com>
2022-05-24 12:14:02 +0800

    fix bug

--- src/views/task/components/normal/NextButton.vue
+++ src/views/task/components/normal/NextButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full relative h-50px list cursor-pointer " v-waves>
-    <div  @click="$emit('click')"
+    <div  @click="$emit('clicks')"
       class="w-170px absolute right-0 h-50px flex items-center justify-center bg-color-[#2B60E9] rounded-[25px] text-color-[#ffffff]">
       {{ $t('common.next') }}
     </div>
