4cee9011915907d760e6a11ce1672940f8f023ce
szfdiwang <szfdiwang@163.com>
2022-05-27 11:49:35 +0800

    fix:bug

---
+++ src/views/workflow/components/ModelEvaluation.vue
@@ -0,0 +1,12 @@
+<template>
+    <el-input v-model="props.modelEvaluate" :rows="10" type="textarea" placeholder="" />
+</template>
+
+<script setup lang='ts'>
+const props = defineProps({
+    modelEvaluate: {
+        type: String,
+        default: ""
+    }
+})
+</script>
